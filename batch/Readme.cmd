2aa4cc935fe0c9de6c8d50edf2f8b7509dfc9aaa
Waleed El-Badry <waleed.elbadry@must.edu.eg>
2016-09-03 23:54:23 +0200

    Update
     - Added windows batch file to simplify running the local server.

---
+++ batch/Readme.cmd
@@ -0,0 +1,17 @@
+# StartBlynkServer.cmd
+
+This batch file allows you to run local server in simpler way. Placing it alongside the server file allows you to:
+
+- Display Server IP address
+- Display application and hardware ports
+- Running the server without the burden of writing the full path of it.
+
+## Note
+Modify the server version to match the version you used
+
+
+`java -jar server-<version>.jar -dataFolder /path`
+
+## Example:
+`java -jar server-0.17.0.jar -dataFolder /path`
+
