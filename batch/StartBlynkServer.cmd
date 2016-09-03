e4d10117e455cff62591cef2f0a20199433737ba
Waleed El-Badry <waleed.elbadry@must.edu.eg>
2016-09-03 23:31:13 +0200

    Update
    - Added batch file for running server in an easier way

---
+++ batch/StartBlynkServer.cmd
@@ -0,0 +1,25 @@
+@echo off
+echo **************************
+echo Starting Blynk Server
+echo Script created by
+echo Waleed El-Badry
+echo waleed.elbadry@must.edu.eg
+echo **************************
+echo Your working directory is %~dp0
+echo **************************
+set ip_address_string="IP Address"
+rem Uncomment the following line when using Windows 7 or Windows 8 / 8.1 (with removing "rem")!
+set ip_address_string="IPv4 Address"
+echo **************************
+for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address_string%`) do (
+    echo Local Server IP Address is: %%f
+    echo Hardware Port : 8442
+	echo Application Port : 8443
+)
+echo **************************
+
+cd /D %~dp0
+java -jar server-0.17.0.jar -dataFolder /path
+IF /I "%ERRORLEVEL%" NEQ "0" (
+    ECHO Server failed to started
+)
